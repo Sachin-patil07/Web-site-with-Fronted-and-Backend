4329784545b5d777e3ec40f4a6224e494485be2f
sachin-patil07 <sachin.patil@dci-student.org>
2024-05-07 04:35:50 +0200

    web-starting1

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hare Krishna hare Rama</h1>
+        <h2>Hari Bol</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
